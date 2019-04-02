d7e2f184adb98f129238442842ab1ba6dfada356
csabaiDomonkos <csabai.dermus@gmail.com>
2019-04-02 22:09:05 +0200

    try fix

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 8000;
+const PORT = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./DB.js');
 const recipeRoute = require('./recipe.route');
-
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || PORT;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
@@ -22,6 +19,6 @@ app.use(bodyParser.json());
 
 app.use('/recipe', recipeRoute);
 
-app.listen(server_port, , server_ip_address, function(){
-  console.log('Server is running on Port:',server_port);
+app.listen(PORT, function(){
+  console.log('Server is running on Port:',PORT);
 });
