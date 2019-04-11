7d79ee0b4f3dec761492d84a1238f148138001cf
csabaiDomonkos <csabai.dermus@gmail.com>
2019-04-11 19:37:10 +0200

    fix

--- server.js
+++ server.js
@@ -7,21 +7,11 @@ const mongoose = require('mongoose');
 const config = require('./DB.js');
 const recipeRoute = require('./recipe.route');
 
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-
-
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)}
 );
-
-app.get('/process', function(req, res){
-	console.log(process.env)
-	res.json(process.env);
-});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
