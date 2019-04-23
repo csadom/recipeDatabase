b35d57205560f2b4260f6e31017b75321708a358
csabaiDomonkos <csabai.dermus@gmail.com>
2019-04-23 21:02:37 +0200

    debug

--- recipe.route.js
+++ recipe.route.js
@@ -59,6 +59,7 @@ recipeRoutes.route('/').get(function (req, res) {
 				console.log(req.body.user);
 				console.log(tokens.get(req.body.user));
 				console.log(req.query);
+				console.log(req.params);
 	if(!req.body.token.localeCompare(tokens.get(req.body.user)) === 0){
 		res.status(401).send("auth error");
 	}else{
